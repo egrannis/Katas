60bc099ce9c715c719c11a19f203f6dca13261b6
Emma Grannis <grannis.emma@gmail.com>
2022-08-22 21:35:59 +0000

    Add instructions and updated passing code to prime number activity

---
+++ primenumber.js
@@ -0,0 +1,23 @@
+/* You will get a numbern (>= 0) and your task is to find the next prime number.
+Make sure to optimize your code: there will numbers tested up to about 10^12. */
+
+const nextPrime = (n) => {
+  let num = n + 1;
+
+  const primeTest = (prime) => {
+    if (prime === 2 || prime === 3)
+      return true;
+    if (prime <= 1 || prime % 2 === 0 || prime % 3 === 0)
+      return false;
+    for (let i = 5; i * i <= prime; i += 6)
+      if (prime % i === 0 || prime % (i + 2) === 0)
+        return false;
+    return true;
+  };
+
+  while (!primeTest(num)) {
+    num++;
+  }
+  return num;
+};
+
