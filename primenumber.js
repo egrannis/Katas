1e076a90f1900c5f3b06db4cfa66114aca5ad7cb
Emma Grannis <grannis.emma@gmail.com>
2022-08-23 15:31:55 +0000

    add refactor of nextprime function

--- primenumber.js
+++ primenumber.js
@@ -21,3 +21,20 @@ const nextPrime = (n) => {
   return num;
 };
 
+////
+
+const isPrime = (n) => {
+  if (n < 2) {
+    return false;
+  }
+
+  for (let i = 2; i <= Math.sqrt(n); i++) {
+    if (n % i === 0) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+const nextPrimeRefactor = (n) => isPrime(n + 1) ? n + 1 : nextPrime(n + 1);
